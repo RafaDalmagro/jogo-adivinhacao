76647afa196bcc461438b5a905384da0dff10031
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-11 13:16:15 -0300

    Finalizando componente de input.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
 import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
 
 export function App() {
     function handleRestartGame() {
@@ -13,14 +14,20 @@ export function App() {
             <main>
                 <Header current={5} max={10} onRestart={handleRestartGame} />
                 <Tip tip="Dica do jogo" />
-            <div className={styles.word}>
-                <Letter value="R" />
-                <Letter value="A" />
-                <Letter value="F" />
-                <Letter value="A" />
-                <Letter value="E" />
-                <Letter value="L" />
-            </div>
+                <div className={styles.word}>
+                    <Letter value="R" />
+                    <Letter value="A" />
+                    <Letter value="F" />
+                    <Letter value="A" />
+                    <Letter value="E" />
+                    <Letter value="L" />
+                </div>
+
+                <h4>Palpite</h4>
+
+                <div>
+                    <Input autoFocus maxLength={1} placeholder="?" />
+                </div>
             </main>
         </div>
     );
