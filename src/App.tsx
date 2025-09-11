0b1903f4ba8eec75861a70dc9c5748a463b966a3
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-11 13:04:39 -0300

    Criando e estilizando componente das letras.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+
 export function App() {
     function handleRestartGame() {
         alert("Reiniciar jogo");
@@ -11,6 +13,14 @@ export function App() {
             <main>
                 <Header current={5} max={10} onRestart={handleRestartGame} />
                 <Tip tip="Dica do jogo" />
+            <div className={styles.word}>
+                <Letter value="R" />
+                <Letter value="A" />
+                <Letter value="F" />
+                <Letter value="A" />
+                <Letter value="E" />
+                <Letter value="L" />
+            </div>
             </main>
         </div>
     );
