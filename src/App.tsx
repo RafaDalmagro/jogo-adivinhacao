bee162dd165ae6fc6167879526fa66315e0ff5f8
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-12 22:16:43 -0300

    Finalizando projeto, necessário corrigir alguns detalhes da intervace.

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,19 @@ import { Button } from "./components/Button";
 import { LettersUsed } from "./components/LettersUsed";
 import type { LettersUsedProps } from "./components/LettersUsed";
 
+const MAX_ATTEMPTS = 5;
+
 export function App() {
-    const [attempt, setAttempt] = useState(0);
+    const [score, setScore] = useState(0);
     const [letter, setLetter] = useState("");
-    const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([
-        { value: "A", correct: true },
-        { value: "B", correct: false },
-    ]);
+    const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([]);
     const [challenge, setChallenge] = useState<Challenge | null>(null);
 
     function handleRestartGame() {
-        alert("Reiniciar jogo");
+        const isConfirmed = window.confirm("Tem certeza que deseja reiniciar?");
+        if (isConfirmed) {
+            startGame();
+        }
     }
 
     function startGame() {
@@ -28,8 +30,14 @@ export function App() {
         const randomWord = WORDS[index];
         setChallenge(randomWord);
 
-        setAttempt(0);
+        setScore(0);
         setLetter("");
+        setLettersUsed([]);
+    }
+
+    function endGame(message: string) {
+        alert(message);
+        startGame();
     }
 
     function handleConfirm() {
@@ -45,10 +53,19 @@ export function App() {
         const exist = lettersUsed.find((l) => l.value.toUpperCase() === value);
 
         if (exist) {
+            setLetter("");
             return alert("Você já tentou essa letra");
         }
 
-        setLettersUsed((prev) => [...prev, { value, correct: false }]);
+        const hits = challenge.word
+            .toUpperCase()
+            .split("")
+            .filter((l) => l === value).length;
+
+        const correct = hits > 0;
+        const currentScore = hits + score;
+
+        setLettersUsed((prev) => [...prev, { value, correct }]);
 
         setLetter("");
     }
@@ -56,6 +73,24 @@ export function App() {
     useEffect(() => {
         startGame();
     }, []);
+
+    useEffect(() => {
+        if (!challenge) {
+            return;
+        }
+
+        setTimeout(() => {
+            if (score === challenge.word.length) {
+                return endGame("Parabéns! Você acertou a palavra!");
+            }
+
+            if (lettersUsed.length === challenge.word.length + MAX_ATTEMPTS) {
+                return endGame(
+                    `Suas tentativas acabaram! A palavra era ${challenge.word}`
+                );
+            }
+        }, 200);
+    }, [score, lettersUsed.length]);
 
     if (!challenge) {
         return;
@@ -65,15 +100,25 @@ export function App() {
         <div className={styles.container}>
             <main>
                 <Header
-                    current={attempt}
-                    max={10}
+                    current={lettersUsed.length}
+                    max={challenge.word.length + MAX_ATTEMPTS}
                     onRestart={handleRestartGame}
                 />
-                <Tip tip="Dica do jogo" />
+                <Tip tip={challenge.tip} />
                 <div className={styles.word}>
-                    {challenge.word.split("").map(() => (
-                        <Letter value="" />
-                    ))}
+                    {challenge.word.split("").map((letter, index) => {
+                        const letterUsed = lettersUsed.find(
+                            (l) =>
+                                l.value.toUpperCase() === letter.toUpperCase()
+                        );
+                        return (
+                            <Letter
+                                key={index}
+                                value={letterUsed?.value}
+                                color={letterUsed?.correct ? "right" : "wrong"}
+                            />
+                        );
+                    })}
                 </div>
 
                 <h4>Palpite</h4>
